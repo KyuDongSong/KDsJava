2e188cab1e79a38d82f1b0ead7e04e9f19b4b586
Admin-PC\Admin <yarn18019@hanmail.net>
2017-01-25 16:21:38 +0900

    재귀호출 Test

---
+++ RecursionCall/src/com/ktds/skd/Main.java
@@ -0,0 +1,8 @@
+package com.ktds.skd;
+
+public class Main {
+	public static void main(String[] args) {
+		Recursion recursion = new Recursion();
+		recursion.call(10);
+	}
+}
---
+++ RecursionCall/src/com/ktds/skd/Recursion.java
@@ -0,0 +1,11 @@
+package com.ktds.skd;
+
+public class Recursion {
+	public void call(int index) {
+		System.out.println("Recursion.call() 호출 중 ...." + index);
+
+		if (index >= 0) {
+			call(--index);
+		}
+	}
+}
